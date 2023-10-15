094c282d63b9f782110d5ee1a57a96aef848e733
Konstantin <kostnk1@gmail.com>
2023-10-15 19:25:11 +0200

    задание 3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
 
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
+        Console.WriteLine("Как Вас зовут?");
+        var Name = Console.ReadLine();
 
+        Console.WriteLine("Сколько вам лет?");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+
+        Console.WriteLine("Какая ваша дата рождения?");
+        var dateBirth = Console.ReadLine();
+
+        Console.WriteLine("Вас зовут {0}, Вам {1} лет, Вы родились {2}", Name, age, dateBirth );
         Console.ReadKey();
 
-
-
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
     }
 }

bd76a8b27def17ac6ac4d9601a10376382339a6c
Konstantin <kostnk1@gmail.com>
2023-10-15 17:29:37 +0200

    Task 3.3.2

--- Program.cs
+++ Program.cs
@@ -5,11 +5,18 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Konstantin";
-
-        Console.WriteLine(myName);
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
 
         Console.ReadKey();
+
+
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
     }
 }

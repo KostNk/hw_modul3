40b9d7ed1c09311d478372ed09054b2c84efb967
Konstantin <kostnk1@gmail.com>
2023-10-21 08:02:54 +0200

    Задание 4.1.17

--- Program.cs
+++ Program.cs
@@ -6,6 +6,33 @@ class MainClass
     public static void Main(string[] args)
     {
 
+        Console.WriteLine("Введите ваш любимый цвет");
+
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
+
         Console.WriteLine("Как Вас зовут?");
         var Name = Console.ReadLine();
 
